5687a3b58e2877d278a3961f23fcbb37f524ca98
YourNeighbor-2890 <79471651+YourNeighbor-2890@users.noreply.github.com>
2021-10-27 15:50:37 -0500

    Delete FiveStarVideo.py

--- FiveStarVideo.py
+++
@@ -1,8 +0,0 @@
-daysNV = int(input("How many days has the new video been rented: "))
-daysOV = int(input("How many days has the old video been rented: "))
-totalNV = daysNV * 3
-print(totalNV)
-totalOV = daysOV * 2
-print(totalOV)
-total = totalOV + totalNV
-print("The total is $", total)
